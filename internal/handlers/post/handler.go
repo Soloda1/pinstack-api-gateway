38a8f54f2e787edd6753a4abecd66706523af967
Soloda1 <enderderder@gmail.com>
2025-07-31 18:00:15 +0400

    fix: добавлен клиент пользователя в обработчик постов

--- internal/handlers/post/handler.go
+++ internal/handlers/post/handler.go
@@ -2,17 +2,20 @@ package post_handler
 
 import (
 	post_client "pinstack-api-gateway/internal/clients/post"
+	user_client "pinstack-api-gateway/internal/clients/user"
 	"pinstack-api-gateway/internal/logger"
 )
 
 type PostHandler struct {
 	postClient post_client.PostClient
+	userClient user_client.UserClient
 	log        *logger.Logger
 }
 
-func NewPostHandler(postClient post_client.PostClient, log *logger.Logger) *PostHandler {
+func NewPostHandler(postClient post_client.PostClient, userCLient user_client.UserClient, log *logger.Logger) *PostHandler {
 	return &PostHandler{
 		postClient: postClient,
+		userClient: userCLient,
 		log:        log,
 	}
 }
