fbd2672229dbb33fccadc3fa4d6bfbe5d5541b97
Soloda1 <enderderder@gmail.com>
2025-08-03 19:04:50 +0400

    fix: обновлены структуры ответов для эндпоинтов получения подписчиков и подписок, добавлены новые поля

--- internal/clients/relation/interface.go
+++ internal/clients/relation/interface.go
@@ -1,10 +1,13 @@
 package relation_client
 
-import "context"
+import (
+	"context"
+	"pinstack-api-gateway/internal/models"
+)
 
 type RelationClient interface {
 	Follow(ctx context.Context, followerID, followeeID int64) error
 	Unfollow(ctx context.Context, followerID, followeeID int64) error
-	GetFollowers(ctx context.Context, followeeID int64, limit, page int32) ([]int64, error)
-	GetFollowees(ctx context.Context, followerID int64, limit, page int32) ([]int64, error)
+	GetFollowers(ctx context.Context, followeeID int64, limit, page int32) ([]*models.RelationUser, int64, error)
+	GetFollowees(ctx context.Context, followerID int64, limit, page int32) ([]*models.RelationUser, int64, error)
 }
--- internal/models/relation.go
+++ internal/models/relation.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"time"
+
+	pb "github.com/soloda1/pinstack-proto-definitions/gen/go/pinstack-proto-definitions/relation/v1"
 )
 
 type Follower struct {
@@ -9,4 +11,18 @@ type Follower struct {
 	FollowerID int64     `json:"follower_id"`
 	FolloweeID int64     `json:"followee_id"`
 	CreatedAt  time.Time `json:"created_at"`
+}
+
+type RelationUser struct {
+	ID        int64   `json:"id"`
+	Username  string  `json:"username"`
+	AvatarURL *string `json:"avatar_url,omitempty"`
+}
+
+func RelationUserFromProto(u *pb.User) *RelationUser {
+	return &RelationUser{
+		ID:        u.FollowerId,
+		Username:  u.Username,
+		AvatarURL: u.AvatarUrl,
+	}
 }
