ad8bb55f99a2f296d7ed199181344d4eec241314
Soloda1 <enderderder@gmail.com>
2025-08-05 16:15:36 +0400

    refactor: обновлен импорт custom_errors на новый путь в нескольких файлах

--- internal/handlers/user/create.go
+++ internal/handlers/user/create.go
@@ -3,9 +3,9 @@ package user_handler
 import (
 	"encoding/json"
 	"errors"
+	"github.com/soloda1/pinstack-proto-definitions/custom_errors"
 	"log/slog"
 	"net/http"
-	"pinstack-api-gateway/internal/custom_errors"
 	"pinstack-api-gateway/internal/models"
 	"pinstack-api-gateway/internal/utils"
 
--- internal/handlers/user/search.go
+++ internal/handlers/user/search.go
@@ -2,8 +2,8 @@ package user_handler
 
 import (
 	"errors"
+	"github.com/soloda1/pinstack-proto-definitions/custom_errors"
 	"net/http"
-	"pinstack-api-gateway/internal/custom_errors"
 	"pinstack-api-gateway/internal/utils"
 	"strconv"
 )
